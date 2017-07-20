45304f2346faced853859de98c3e7f99f5c2e9de
kiyokuro <kuro6412@gmail.com>
2017-07-20 16:32:19 +0900

    valid?を検証するテストを追加

--- lib/japanese_email_domain.rb
+++ lib/japanese_email_domain.rb
@@ -3,15 +3,16 @@ require "japanese_email_domain/version"
 module JapaneseEmailDomain
 
   class Validator
-    valid_mail_regexp = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    mail_domains = %W(docomo.ne.jp ezweb.ne.jp softbank.ne.jp i.softbank.jp)
+    VALID_MAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    MAIL_DOMAINS = %W(docomo.ne.jp ezweb.ne.jp softbank.ne.jp i.softbank.jp)
 
     def initialize(address)
-      return false unless address.match(valid_mail)
+      @address = address
       @account, @domain = address.split('@')
     end
     def valid?
-      mail_domains.include?(@domain)
+      return false unless @address.match(VALID_MAIL_REGEXP)
+      MAIL_DOMAINS.include?(@domain)
     end
   end
 end
--- spec/japanese_email_domain_spec.rb
+++ spec/japanese_email_domain_spec.rb
@@ -1,11 +1,38 @@
 require "spec_helper"
+require 'japanese_email_domain'
 
 RSpec.describe JapaneseEmailDomain do
-  it "has a version number" do
-    expect(JapaneseEmailDomain::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'validate' do
+    let!(:email){ JapaneseEmailDomain::Validator.new(param) }
+
+    context 'when blank invalid' do
+      let(:param){ ' ' }
+      it { expect(email.valid?).to eq false }
+    end
+    context 'when not has @ invalid' do
+      let(:param){ 'japanese.com' }
+      it { expect(email.valid?).to eq false }
+    end
+    context 'when not has account part to invalid' do
+      let(:param){ '@docomo.ne.jp' }
+      it { expect(email.valid?).to eq false }
+    end
+    context 'when correct docomo email to vaild' do
+      let(:param){ 'japanese@docomo.ne.jp' }
+      it { expect(email.valid?).to eq true }
+    end
+    context 'when correct au email to vaild' do
+      let(:param){ 'japanese@ezweb.ne.jp' }
+      it { expect(email.valid?).to eq true }
+    end
+    context 'when correct softbank email to vaild' do
+      let(:param){ 'japanese@softbank.ne.jp' }
+      it { expect(email.valid?).to eq true }
+    end
+    context 'when correct softbank iphone email to vaild' do
+      let(:param){ 'japanese@i.softbank.jp' }
+      it { expect(email.valid?).to eq true }
+    end
   end
 end
