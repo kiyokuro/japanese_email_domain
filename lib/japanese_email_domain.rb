794b35025f9084630a590bae4cde529ffa370714
kiyokuro <kuro6412@gmail.com>
2017-07-20 12:00:52 +0900

    基本的なバリデート機能を追加

--- lib/japanese_email_domain.rb
+++ lib/japanese_email_domain.rb
@@ -1,5 +1,17 @@
 require "japanese_email_domain/version"
 
 module JapaneseEmailDomain
-  # Your code goes here...
+
+  class Validator
+    valid_mail_regexp = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    mail_domains = %W(docomo.ne.jp ezweb.ne.jp softbank.ne.jp i.softbank.jp)
+
+    def initialize(address)
+      return false unless address.match(valid_mail)
+      @account, @domain = address.split('@')
+    end
+    def valid?
+      mail_domains.include?(@domain)
+    end
+  end
 end
